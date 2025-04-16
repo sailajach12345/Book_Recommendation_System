74a1b4b863c1025192035dd30b12e95ed67dd064
sailaja ch <sailchepuri@gmail.com>
2025-04-16 20:26:26 +0530

    restuructured to deploy on vercel

--- API/app.py
+++ API/app.py
@@ -1,11 +1,38 @@
 from flask import Flask, request, jsonify
-from recommender import recommend_books
+#from recommender import recommend_books
+import numpy as np
+import pickle
 
 app = Flask(__name__)
 
+
+# Load Pickle
+with open("recommender_model.pkl", "rb") as file:
+    data = pickle.load(file)
+
+#print(data)
+pt = data.get('pt_data')
+similarity_score = data.get('similarity_matrix')
+books = data.get('books')
+def recommend_books(book_name):
+    index = np.where(pt.index==book_name)[0][0]
+    similar_books = sorted(list(enumerate(similarity_score[index])),key=lambda x:x[1], reverse=True)[1:6]
+
+    data = []
+
+    for i in similar_books:
+        item = []
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+        data.append(item)
+    return data
+
 @app.route('/welcome')
 def home():
-    return "📚 Welcome to the Book Recommendation API!"
+    return "📚 Welcome to the Book Recommendation api!"
 
 @app.route('/recommend', methods=['GET'])
 def recommend():
